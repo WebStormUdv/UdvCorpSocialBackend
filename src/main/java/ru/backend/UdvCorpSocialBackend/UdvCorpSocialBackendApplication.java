a18440568ce9050adb249631835797fa0623f51e
Dbatr <bdanis2311@gmail.com>
2025-04-25 19:53:45 +0500

    init commit

---
+++ src/main/java/ru/backend/UdvCorpSocialBackend/UdvCorpSocialBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.backend.UdvCorpSocialBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdvCorpSocialBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdvCorpSocialBackendApplication.class, args);
+	}
+
+}
